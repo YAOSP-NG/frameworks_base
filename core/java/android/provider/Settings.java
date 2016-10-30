aa423fcfe9ea0ce8bb22a411a958d9b68022bb10
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-30 19:59:51 +0100

    [1/2] frameworks_base: BatteryLight: add color for fast charging
    Also review the charging LED logic to always display charging (normal/fast) as soon as
    charging begins, even if in low level (so don't use low color while charging anymore)
    
    Thanx to Dankoman for suggesting to add this ;)
    
    Includes :
    
    BatteryLight: use local config for fast charging threshold
    
    Grabbing it from Keyguard resources doesn't seem to work on all supported devices
    
    This is a workaround for the time being
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3606,16 +3606,22 @@ public final class Settings {
         public static final String DOUBLE_TAP_WAKE_DOZE = "double_tap_wake_doze";
 
         /**
-         * What color to use for the battery LED while charging - low
+         * What color to use for the battery LED while low
          * @hide
          */
         public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
 
         /**
-         * What color to use for the battery LED while charging - medium
+         * What color to use for the battery LED while charging - slow or normal
          * @hide
          */
         public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+        /**
+         * What color to use for the battery LED while charging - fast
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_MEDIUM_FAST_COLOR = "battery_light_medium_fast_color";
 
         /**
          * What color to use for the battery LED while charging - full
