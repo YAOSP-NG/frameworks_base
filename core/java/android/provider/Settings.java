5c8e004c9fc2b35e3495568b0e3011e3d00c6ef0
GWX <kagwind@gmail.com>
2016-10-31 12:01:14 +0100

    [1/3] frameworks_base: Flip to Mute/Reject Call
    saving flip action value to Setings container

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3775,6 +3775,14 @@ public final class Settings {
                 "proximity_auto_speaker_incall_only";
 
         /**
+         * Whether flip action during incomming call should mute or dismiss
+         * the call (mute = 0, dismiss = 1, nothing = 2 (default))
+         *
+         * @hide
+         */
+        public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
