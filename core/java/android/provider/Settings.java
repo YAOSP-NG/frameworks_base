df8bb8dabd5ed43b53a8261da74c13365e7709a5
maxwen <max.weninger@gmail.com>
2016-11-06 18:18:46 +0100

    base: SystemUI: make QS columns count configurable
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    PS4
    we must explicitly hide the columns submenu
    
    Change-Id: Id033a5a2bcd983d7704923e760bb8941c91be013

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3873,6 +3873,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Quick Settings number of culumns 
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -823,4 +823,8 @@ public class StatusBarHeaderView extends BaseStatusBarHeader implements View.OnC
                     .start();
         }
     };
+
+    @Override
+    public void updateSettings() {
+    }
 }
